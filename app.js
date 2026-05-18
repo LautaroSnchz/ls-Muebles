3f76869089a7c34b5c1ea6eb67fe7ee8fc664696
LautaroSnchz <lautaro.snchz94@gmail.com>
2026-05-18 19:42:12 -0300

    feat: implementar carrito dinamico, ventana modal y mejoras UI

--- app.js
+++ app.js
@@ -40,6 +40,81 @@ stockProductos.forEach((producto) => {
     })
 })
 
+const agregarAlCarrito = (prodId) => {
+    const existe = carrito.some(prod => prod.id === prodId);
+    if (existe) {
+        carrito.forEach(prod => {
+            if (prod.id === prodId) {
+                prod.cantidad++;
+            }
+        });
+    } else {
+        const item = stockProductos.find((prod) => prod.id === prodId);
+        if (item) {
+            carrito.push({...item, cantidad: 1});
+        }
+    }
+    actualizarCarrito();
+}
+
+const actualizarCarrito = () => {
+    const contenedorCarrito = document.getElementById('carrito-contenedor');
+    if (contenedorCarrito) {
+        contenedorCarrito.innerHTML = '';
+        carrito.forEach((prod) => {
+            const div = document.createElement('div');
+            div.className = 'productoEnCarrito';
+            div.innerHTML = `
+                <p>${prod.nombre}</p>
+                <p>Precio: $${prod.precio}</p>
+                <p>Cantidad: <span>${prod.cantidad}</span></p>
+                <button onclick="eliminarDelCarrito(${prod.id})" class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>
+            `;
+            contenedorCarrito.appendChild(div);
+        });
+    }
+
+    const contadorCarrito = document.getElementById('contadorCarrito');
+    if (contadorCarrito) {
+        contadorCarrito.innerText = carrito.reduce((acc, prod) => acc + prod.cantidad, 0);
+    }
+    
+    const precioTotal = document.getElementById('precioTotal');
+    if (precioTotal) {
+        precioTotal.innerText = carrito.reduce((acc, prod) => acc + (prod.precio * prod.cantidad), 0);
+    }
+    
+    localStorage.setItem('carrito', JSON.stringify(carrito));
+}
+
+const eliminarDelCarrito = (prodId) => {
+    const item = carrito.find((prod) => prod.id === prodId);
+    const indice = carrito.indexOf(item);
+    carrito.splice(indice, 1);
+    actualizarCarrito();
+}
+
+const botonCarrito = document.getElementById('boton-carrito');
+const modalCarrito = document.getElementById('modal-carrito');
+const botonCerrar = document.getElementById('carritoCerrar');
+const vaciarCarrito = document.getElementById('vaciar-carrito');
+
+if (botonCarrito && modalCarrito) {
+    botonCarrito.addEventListener('click', () => {
+        modalCarrito.classList.add('modal-active');
+    });
+}
+if (botonCerrar && modalCarrito) {
+    botonCerrar.addEventListener('click', () => {
+        modalCarrito.classList.remove('modal-active');
+    });
+}
+if (vaciarCarrito) {
+    vaciarCarrito.addEventListener('click', () => {
+        carrito.length = 0;
+        actualizarCarrito();
+    });
+}
 
 //--------------------------------------------
 //js para el formu 
